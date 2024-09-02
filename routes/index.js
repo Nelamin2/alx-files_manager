d94c4a75adce064282ebd1588f326a9d322c2c90
Nelamin2 <nidalelamin@gmail.com>
2024-09-03 02:09:39 +0300

    tasks

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,22 @@
-// routes/index.js
-const express = require('express');
-const router = express.Router();
-const AppController = require('../controllers/AppController');
+import { Router } from 'express';
+import AppController from '../controllers/AppController';
+import AuthController from '../controllers/AuthController';
+import UsersController from '../controllers/UsersController';
+import FilesController from '../controllers/FilesController';
 
-// Define routes and map them to controller methods
+const router = Router();
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files/:id/data', FilesController.getFile);
+router.get('/files', FilesController.getIndex)
+router.get('/stats', AppController.getStats);
+router.post('/users', UsersController.postNew);
+router.post('/files', FilesController.postUpload);
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
 
-module.exports = router;
-
+export default router;
--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 import express from 'express';
-import controllerRouting from './routes/index';
+import routes from './routes';
 
-const app = express();
 const port = process.env.PORT || 5000;
+const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(routes);
 
-controllerRouting(app);
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+app.listen(port, () => console.log(`Server running on ${port}`));
 
 export default app;
