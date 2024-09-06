cab1fb456972f4d51de9ed563eaa723cc93c0c01
Nelamin2 <nidalelamin@gmail.com>
2024-09-07 02:11:00 +0300

    tasks

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,6 @@ import FilesController from '../controllers/FilesController';
 
 const router = Router();
 router.get('/status', AppController.getStatus);
-router.get('/stats', AppController.getStats);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
